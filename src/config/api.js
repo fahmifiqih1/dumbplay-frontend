c4a736c7e201291a39ac8df97d4340636f7c9cb6
fahmifiqih <53596721+fahmifiqih1@users.noreply.github.com>
2021-11-09 17:22:55 +0700

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // Set config defaults when creating the instance
 export const API = axios.create({
-	baseURL: 'https://api.play.onlinecamp.id/api/v1'
+	baseURL: 'http://api.play.onlinecamp.id/api/v1'
 });
 
 // Alter defaults after instance has been created
